ac85c006387dac831c07cf48f9521612ccb8d33a
EVOKE <evoke.lektrique@gmail.com>
2017-09-26 15:17:00 +0330

    Update example.rb

--- example.rb
+++ example.rb
@@ -1,13 +1,13 @@
 require_relative 'core' 
 
 Config.setup do |config|
-  config.username 				= 'evoke'
-  config.password 				= '1234567'
-  config.max_likes_per_day 		= '5'
-  config.max_follow_per_day 	= '5'
+  config.username 				      = 'evoke'
+  config.password 				      = '1234567'
+  config.max_likes_per_day 		  = '5'
+  config.max_follow_per_day 	  = '5'
   config.max_comments_per_day 	= '5'
-  config.unwanted_list 			= ["_id","free_","free"]
-  config.white_list_users 		= ["john","alex","nick"]
+  config.unwanted_list 			    = ["_id","free_","free"]
+  config.white_list_users 		  = ["john","alex","nick"]
 end
 
 instagram = Instabot.new
