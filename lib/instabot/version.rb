4281faba2079a357ea182087978b7a616841db76
EVOKE <evoke.lektrique@gmail.com>
2017-12-04 19:04:00 +0330

    Update version.rb

--- lib/instabot/version.rb
+++ lib/instabot/version.rb
@@ -1,5 +1,5 @@
 module Version
-  VERSION     = '0.1.40'.freeze
+  VERSION     = '0.1.50'.freeze
   SUMMARY     = 'Ruby instagram bot'.freeze
   DESCRIPTION = 'A instagram bot works without instagram api, only needs your username and password'.freeze
 end
