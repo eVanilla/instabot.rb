37d7b02182b534d402a3bdfcbfbafc285b3fa809
megosparta <megosparta@mail.ru>
2017-12-18 00:04:07 +0300

    Refactoring code with media and user informations

--- lib/instabot/core.rb
+++ lib/instabot/core.rb
@@ -19,7 +19,7 @@ class Instabot
     include Log
     include Protocol
 
-    attr_accessor :users, :medias, :informations
+    attr_accessor :users, :medias, :media_informations, :user_informations
 
     def initialize(mode = :default)
         @login_mode    = mode 
--- lib/instabot/grabber.rb
+++ lib/instabot/grabber.rb
@@ -1,5 +1,5 @@
 module Grabber
-  def get_user_informations(user_id)
+  def handle_user_informations_data(user_id)
     puts '[+] '.cyan + "Trying to get user (#{user_id}) informations"
     log("Trying to get user (#{user_id}) informations", 'GRABBER')
     user_page = "https://www.instagram.com/web/friendships/#{user_id}/follow/"
@@ -9,7 +9,34 @@ module Grabber
     response  = @agent.get("https://instagram.com/#{username}/?__a=1")
     data      = parse_response(response.body)
 
-    @informations = {
+    set_user_informations(data)
+  end
+
+  def handle_media_informations_data(media_id)
+    puts '[+] '.cyan + "Trying to get media (#{media_id}) informations"
+    log("Trying to get media (#{media_id}) informations", 'GRABBER')
+    response = @agent.get("https://www.instagram.com/p/#{media_id}/?__a=1")
+    data     = parse_response(response.body)
+
+    set_media_informations(data)
+
+    unless @infinite_tags == true
+      tags = @media_informations[:text].encode('UTF-8', invalid: :replace, undef: :replace, replace: '?').split(/\W+/)
+      id   = 0
+      tags.each do |tag|
+        if tag == '_' || tag == '' || tag.nil?
+          tags.delete(tag)
+        else
+          id += 1
+          Config.options.tags << tag
+        end
+      end
+      puts '[+] '.cyan + '[' + id.to_s.yellow + '] New tags added'
+    end
+  end
+
+  def set_user_informations(data)
+    @user_informations = {
       followers:   data.deep_find('followed_by')['count'],
       following:   data.deep_find('follows')['count'],
       is_private:  data.deep_find('is_private'),
@@ -21,13 +48,8 @@ module Grabber
     }
   end
 
-  def get_media_informations(media_id)
-    puts '[+] '.cyan + "Trying to get media (#{media_id}) informations"
-    log("Trying to get media (#{media_id}) informations", 'GRABBER')
-    response = @agent.get("https://www.instagram.com/p/#{media_id}/?__a=1")
-    data     = parse_response(response.body)
-
-    @informations = {
+  def set_media_informations(data)
+    @media_informations = {
       id:                  data.deep_find('id'),
       full_name:           data.deep_find('full_name'),
       owner:               data.deep_find('owner')['username'],
@@ -41,20 +63,6 @@ module Grabber
       requested_by_viewer: data.deep_find('requested_by_viewer'),
       text:                data.deep_find('text') 
     }
-
-    unless @infinite_tags == true
-      tags = @informations[:text].encode('UTF-8', invalid: :replace, undef: :replace, replace: '?').split(/\W+/)
-      id   = 0
-      tags.each do |tag|
-        if tag == '_' || tag == '' || tag.nil?
-          tags.delete(tag)
-        else
-          id += 1
-          Config.options.tags << tag
-        end
-      end
-      puts '[+] '.cyan + '[' + id.to_s.yellow + '] New tags added'
-    end
   end
 
   def search(tags = [])
